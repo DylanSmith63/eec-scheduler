3aa2361ea700d7840318eeff8f353c2bd77a5379
Dylan Smith <dylanmsmith.63@gmail.com>
2025-03-28 12:13:19 -0500

    earliest deadline first finalized

--- Scheduler.hpp
+++ Scheduler.hpp
@@ -9,12 +9,16 @@
 #define Scheduler_hpp
 
 #include <vector>
+#include <iostream>
+#include <queue>
 
 #include "Interfaces.h"
+#include <queue>
+#include <utility>
 
 class Scheduler {
 public:
-    Scheduler()                 {}
+    Scheduler(){}
     void Init();
     void MigrationComplete(Time_t time, VMId_t vm_id);
     void NewTask(Time_t now, TaskId_t task_id);
@@ -22,8 +26,12 @@ public:
     void Shutdown(Time_t now);
     void TaskComplete(Time_t now, TaskId_t task_id);
 private:
-    vector<VMId_t> vms;
-    vector<MachineId_t> machines;
+    unsigned get_id(MachineState_t state, CPUType_t cpu_type, bool gpu_enabled);
+    unsigned get_virtual_id(VMType_t vm_state, SLAType_t state, CPUType_t cpu_type, bool gpu_enabled);
+    void MonitorQueue(unsigned vm_id);
+    vector<vector<MachineId_t>> machines;
+    vector<vector<VMId_t>> vms;
+    vector<priority_queue<pair<Time_t, TaskId_t>>> queues;
 };
 
 
